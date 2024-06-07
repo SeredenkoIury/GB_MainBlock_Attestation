92c083e48c535d2943ab1be6e9c3e34ee3370cb6
Iury Seredenko <iury.seredenko@gmail.com>
2024-06-07 18:04:44 +0300

    Main programm been finished and tested, added a check if there is empty adding from the console

--- Main_Prog/Program.cs
+++ Main_Prog/Program.cs
@@ -2,9 +2,7 @@
 Console.WriteLine("Main Block Attestation");
 Console.WriteLine("by Seredenko Iury Danilovich https://github.com/SeredenkoIury/GB_MainBlock_Attestation");
 Console.WriteLine("");
-
-
-
+//==========================================================================================================
 // Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
 // длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
@@ -15,13 +13,17 @@ Console.WriteLine("");
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+//==========================================================================================================
 
 // input the length of the 1st array (unchecked)
 Console.WriteLine("Add the number of words in your Array and Press ENTER :");
 int n = int.Parse(Console.ReadLine());
 
+
 // Createing and filling the First Array
 string[] FirstArray = new string[n];
+
+
 for (int i = 0; i < n; i++)
 {
     // Filling the Array from the console with checking if for empty
@@ -30,29 +32,33 @@ for (int i = 0; i < n; i++)
 
     while (string.IsNullOrEmpty(FirstArray[i]))
     {
-        Console.WriteLine("!WARNING! You cannot leave an empty value in the array, Please try again and press Enter");
+        Console.WriteLine("!WARNING!------You cannot leave an empty value in the array.------!WARNING!"); 
+        Console.Write("Please type the word again and press Enter:  ");
         FirstArray[i] = Console.ReadLine();
     }
 }
 
-// Createing the Second Array for words witn 3 or less symbols and the index for 2nd array
+// Createing the Second Array for words with 3 or less symbols and the index for 2nd array
 string[] SecondArray = new string[n];
 int secondArrayIndex = 0;
 
 // Sorting 3 and less, adding to the second array and writing it
-Console.WriteLine("Words with 3 or less symbols:");
+
 for (int i = 0; i < n; i++)
 {
     if (FirstArray[i].Length <= 3)
     {
-        // Добавляем его во второй массив и увеличиваем индекс второго массива
+        
         SecondArray[secondArrayIndex] = FirstArray[i];
         secondArrayIndex++;
     }
 }
 
 // Writing the Second Array
+Console.WriteLine("");
+Console.Write("Words with 3 or less symbols: ");
+
 for (int i = 0; i < secondArrayIndex; i++)
 {
-    Console.Write($" {SecondArray[i]} ");
+    Console.Write($"|  {SecondArray[i]}  |");
 }
